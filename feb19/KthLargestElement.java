997079cb96aa3c1daba8f089e1159356c371ab15
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-19 22:09:58 +0530

    commit 1

---
+++ feb19/KthLargestElement.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class KthLargestElement {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {7, 10, 4, 3, 20, 15};
+        int k = 3;
+        System.out.println("Kth Largest: " + findKthLargest(nums, k));
+    }
+}
+
