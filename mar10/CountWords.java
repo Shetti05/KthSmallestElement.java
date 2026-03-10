4310452b30d144ace7d5ec38d05c545160bb4211
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-10 19:49:58 +0530

    commit 4

---
+++ mar10/CountWords.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        String[] words = str.split(" ");
+        System.out.println("Word Count: " + words.length);
+    }
+}
