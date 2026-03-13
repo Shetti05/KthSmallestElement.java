1ee9da496596c661190a5b91358a6a19a7823bba
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-13 20:50:36 +0530

    commit 3

---
+++ mar13/ReverseWords.java
@@ -0,0 +1,10 @@
+public class ReverseWords {
+    public static void main(String[] args){
+        String str = "Hello Java World";
+        String[] words = str.split(" ");
+
+        for(int i=words.length-1;i>=0;i--){
+            System.out.print(words[i] + " ");
+        }
+    }
+}
