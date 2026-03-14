ebdd2e30c3caaa384ffb51a273a7e700098d9c2d
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-14 19:20:11 +0530

    commit 7

---
+++ mar14/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int arr[]={1,2,2,3,4,4,5};
+
+        Set<Integer> set=new LinkedHashSet<>();
+
+        for(int i:arr)
+            set.add(i);
+
+        System.out.println(set);
+    }
+}import java.util.*;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int arr[]={1,2,2,3,4,4,5};
+
+        Set<Integer> set=new LinkedHashSet<>();
+
+        for(int i:arr)
+            set.add(i);
+
+        System.out.println(set);
+    }
+}
