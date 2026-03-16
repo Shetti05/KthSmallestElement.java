68dbe3f1779624d6700e6eb79f740e50fa4c50c0
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-16 21:30:44 +0530

    commit 5

---
+++ mar16/ReverseWords.java
@@ -0,0 +1,9 @@
+public class ReverseWords {
+    public static void main(String[] args){
+        String s = "cloud devops engineer";
+        String[] words = s.split(" ");
+
+        for(int i=words.length-1;i>=0;i--)
+            System.out.print(words[i] + " ");
+    }
+}
