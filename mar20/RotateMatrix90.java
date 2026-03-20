b25da9cc1caab3c2279a6f45fee55d494f8bac7d
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-20 18:38:27 +0530

    commit 2

---
+++ mar20/RotateMatrix90.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class RotateMatrix90 {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int[] row : matrix) {
+            for (int i = 0; i < n/2; i++) {
+                int temp = row[i];
+                row[i] = row[n-i-1];
+                row[n-i-1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
