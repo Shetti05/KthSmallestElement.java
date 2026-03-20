514d9b14f7a28cf314c537f48d189987a13287db
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-20 18:38:51 +0530

    commit 3

---
+++ mar20/FindPeakElement.java
@@ -0,0 +1,20 @@
+public class FindPeakElement {
+    public static int findPeak(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid+1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,5,6,4};
+        System.out.println(findPeak(nums));
+    }
+}
