ccffb9437cf4b5f4c3e76ef5d30e1f2673823ae8
Jaideep <jaideepshetti55@gmail.com>
2026-03-22 17:16:42 +0530

    commit 4

---
+++ mar22/KthLargest.java
@@ -0,0 +1,18 @@
+// File: KthLargest.java
+import java.util.*;
+
+public class KthLargest {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) {
+            pq.add(n);
+            if (pq.size() > k) pq.poll();
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(findKthLargest(nums, 2));
+    }
+}
