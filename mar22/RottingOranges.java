793d3813f95c18ac5d310adc0d6ca3dfaa7106a0
Jaideep <jaideepshetti55@gmail.com>
2026-03-22 17:18:05 +0530

    commit 6

---
+++ mar22/RottingOranges.java
@@ -0,0 +1,33 @@
+// File: RottingOranges.java
+import java.util.*;
+
+public class RottingOranges {
+    public static int orangesRotting(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        int fresh = 0, time = 0;
+
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) q.add(new int[]{i, j});
+                if (grid[i][j] == 1) fresh++;
+            }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        while (!q.isEmpty() && fresh > 0) {
+            int size = q.size();
+            time++;
+            while (size-- > 0) {
+                int[] cur = q.poll();
+                for (int[] d : dirs) {
+                    int x = cur[0] + d[0], y = cur[1] + d[1];
+                    if (x>=0 && y>=0 && x<grid.length && y<grid[0].length && grid[x][y]==1) {
+                        grid[x][y] = 2;
+                        fresh--;
+                        q.add(new int[]{x,y});
+                    }
+                }
+            }
+        }
+        return fresh == 0 ? time : -1;
+    }
+}
