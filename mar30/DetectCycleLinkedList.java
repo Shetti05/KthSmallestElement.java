187c3152841c1b024f6ee92f63a05c9a83c726c6
Jaideep <jaideepshetti55@gmail.com>
2026-03-30 22:12:13 +0530

    commit 3

---
+++ mar30/DetectCycleLinkedList.java
@@ -0,0 +1,20 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+
+public class DetectCycleLinkedList {
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head, fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+}
