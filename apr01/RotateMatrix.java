f4d70f44c445d6e4d54e0d6560d4ef7b50a5972e
Jaideep <jaideepshetti55@gmail.com>
2026-04-01 20:15:20 +0530

    commit 4

---
+++ apr01/RotateMatrix.java
@@ -0,0 +1,21 @@
+public class RotateMatrix {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int[] row : matrix) {
+            for (int i = 0; i < n / 2; i++) {
+                int temp = row[i];
+                row[i] = row[n - i - 1];
+                row[n - i - 1] = temp;
+            }
+        }
+    }
+}
