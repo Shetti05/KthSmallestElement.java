9d0224c3a4f5ba52607ce8f1cbb9db52336fcfd4
Jaideep <jaideepshetti55@gmail.com>
2026-04-02 14:36:31 +0530

    commit 8

---
+++ apr02/ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+    public static String reverse(String s) {
+        char[] arr = s.toCharArray();
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+        return new String(arr);
+    }
+}
