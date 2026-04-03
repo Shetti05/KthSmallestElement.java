994adbdd62b08ff376cc836546501e82f62d8ed4
Jaideep <jaideepshetti55@gmail.com>
2026-04-03 20:46:30 +0530

    commit 7

---
+++ apr03/TopKFrequentElements.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class TopKFrequentElements {
+    public static int[] topK(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int n : nums) map.put(n, map.getOrDefault(n,0)+1);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a,b)->map.get(b)-map.get(a)
+        );
+
+        pq.addAll(map.keySet());
+        int[] res = new int[k];
+
+        for (int i = 0; i < k; i++) res[i] = pq.poll();
+        return res;
+    }
+}
