01d4ff9ea063195ef7c8c0acc44e70a0331583cf
Jaideep <jaideepshetti55@gmail.com>
2026-04-08 21:25:26 +0530

    commit 8

---
+++ apr08/BinarySearch.java
@@ -0,0 +1,19 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int target = 4, left = 0, right = arr.length - 1;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(arr[mid] == target) {
+                System.out.println(mid);
+                return;
+            }
+            else if(arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+    }
+}
