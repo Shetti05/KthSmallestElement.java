972ab149b59be751a906a84fccbdc788f41e252d
Jaideep <jaideepshetti55@gmail.com>
2026-04-14 22:26:56 +0530

    commit 7

---
+++ apr14/FrequencyArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class FrequencyArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,3,3,3};
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : arr) {
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        System.out.println(map);
+    }
+}
