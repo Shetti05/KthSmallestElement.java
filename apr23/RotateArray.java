dc910deccb1310c722241a28bebdcb66cb085e5d
Jaideep <jaideepshetti55@gmail.com>
2026-04-23 22:15:59 +0530

    commit 10

---
+++ apr23/RotateArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class RotateArray {
+    public static void rotate(int[] arr, int k) {
+        k = k % arr.length;
+        reverse(arr,0,arr.length-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,arr.length-1);
+    }
+
+    static void reverse(int[] arr,int l,int r) {
+        while(l<r) {
+            int t=arr[l]; arr[l]=arr[r]; arr[r]=t;
+            l++; r--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        rotate(arr,2);
+        System.out.println(Arrays.toString(arr));
+    }
+}
