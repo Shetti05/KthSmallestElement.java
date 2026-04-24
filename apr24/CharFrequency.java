f0e96d12069491161b54391771ff6b762a21ff31
Jaideep <jaideepshetti55@gmail.com>
2026-04-24 20:28:17 +0530

    commit 2

---
+++ apr24/CharFrequency.java
@@ -0,0 +1,16 @@
+public class CharFrequency {
+    public static void main(String[] args) {
+        String str = "programming";
+        int[] freq = new int[256];
+
+        for(char c : str.toCharArray()) {
+            freq[c]++;
+        }
+
+        for(int i = 0; i < 256; i++) {
+            if(freq[i] > 0) {
+                System.out.println((char)i + " -> " + freq[i]);
+            }
+        }
+    }
+}
