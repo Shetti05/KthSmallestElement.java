acf6c87f2c348b8e9ca567b37e0f6763e8cc7d63
Jaideep <jaideepshetti55@gmail.com>
2026-04-26 19:18:49 +0530

    commit 6

---
+++ apr26/CountWords.java
@@ -0,0 +1,7 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String s = "Hello world Java";
+        String[] words = s.split(" ");
+        System.out.println(words.length);
+    }
+}
