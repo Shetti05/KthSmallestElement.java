ddbb77b0b509e7ee0cf96ff025b3344e3b476f4d
Jaideep <jaideepshetti55@gmail.com>
2026-05-01 21:28:38 +0530

    commit 4

---
+++ may01/SystemInfo.java
@@ -0,0 +1,6 @@
+public class SystemInfo {
+    public static void main(String[] args) {
+        System.out.println("OS: " + System.getProperty("os.name"));
+        System.out.println("User: " + System.getProperty("user.name"));
+    }
+}
