315a50d5bdfd1a127d6c2c18061c3d343c292535
Jaideep <jaideepshetti55@gmail.com>
2026-05-02 20:48:15 +0530

    commit 5

---
+++ may02/EmailSender.java
@@ -0,0 +1,9 @@
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
+public class EmailSender {
+    public static void main(String[] args) {
+        System.out.println("Setup JavaMail API to send email");
+    }
+}
