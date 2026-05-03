e492e7c1117cd2b1a7e5eab9080c411676886a7d
Jaideep <jaideepshetti55@gmail.com>
2026-05-03 21:42:15 +0530

    commit 9

---
+++ may03/PrimeChecker.java
@@ -0,0 +1,12 @@
+public class PrimeChecker {
+    public static void main(String[] args) {
+        int n = 7;
+        boolean isPrime = true;
+
+        for(int i=2;i<n;i++){
+            if(n%i==0) isPrime=false;
+        }
+
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
