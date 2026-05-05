77012dc0646f1f8fdabaffe9aefc183fdbb888ea
Jaideep <jaideepshetti55@gmail.com>
2026-05-05 21:39:17 +0530

    commit 115

---
+++ may05/PalindromeCheck.java
@@ -0,0 +1,8 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = new StringBuilder(str).reverse().toString();
+
+        System.out.println(str.equals(rev) ? "Palindrome" : "Not Palindrome");
+    }
+}
