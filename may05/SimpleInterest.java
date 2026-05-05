83a364e98bf36512f0397f98b2dad86d4267ca23
Jaideep <jaideepshetti55@gmail.com>
2026-05-05 21:35:58 +0530

    commit 8

---
+++ may05/SimpleInterest.java
@@ -0,0 +1,8 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        int p = 1000, t = 2;
+        float r = 5;
+        float si = (p * t * r)/100;
+        System.out.println("SI: " + si);
+    }
+}
