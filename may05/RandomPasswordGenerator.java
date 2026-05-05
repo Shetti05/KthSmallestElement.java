803cdbce6bd5ea2c05e567ff1daad9c0ff745486
Jaideep <jaideepshetti55@gmail.com>
2026-05-05 21:40:52 +0530

    commit 18

---
+++ may05/RandomPasswordGenerator.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class RandomPasswordGenerator {
+    public static void main(String[] args) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+        Random r = new Random();
+        String pass = "";
+
+        for(int i=0;i<8;i++){
+            pass += chars.charAt(r.nextInt(chars.length()));
+        }
+
+        System.out.println("Password: " + pass);
+    }
+}
