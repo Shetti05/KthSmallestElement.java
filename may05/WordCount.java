0f9b86a3cb3dbf815929356a3e43ec5d4da942fc
Jaideep <jaideepshetti55@gmail.com>
2026-05-05 21:38:32 +0530

    commit 13

---
+++ may05/WordCount.java
@@ -0,0 +1,7 @@
+public class WordCount {
+    public static void main(String[] args) {
+        String text = "Java is powerful and simple";
+        String[] words = text.split(" ");
+        System.out.println("Words: " + words.length);
+    }
+}
