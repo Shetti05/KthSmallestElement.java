5d2cd7e5945749f8f992edbdd7ecd429b1b69f99
Jaideep <jaideepshetti55@gmail.com>
2026-05-06 22:52:53 +0530

    commit 3

---
+++ may06/BinaryToDecimal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter binary number: ");
+        String binary = sc.nextLine();
+
+        int decimal = Integer.parseInt(binary, 2);
+
+        System.out.println("Decimal = " + decimal);
+    }
+}
