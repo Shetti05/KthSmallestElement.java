26248a083658905943e2b70db3870c8b07dabe30
Jaideep <jaideepshetti55@gmail.com>
2026-05-06 22:53:19 +0530

    commit 4

---
+++ may06/SimpleInterest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double p, r, t, si;
+
+        System.out.print("Principal: ");
+        p = sc.nextDouble();
+
+        System.out.print("Rate: ");
+        r = sc.nextDouble();
+
+        System.out.print("Time: ");
+        t = sc.nextDouble();
+
+        si = (p * r * t) / 100;
+
+        System.out.println("Simple Interest = " + si);
+    }
+}
