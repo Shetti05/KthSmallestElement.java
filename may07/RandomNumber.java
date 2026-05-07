92a42bace2dfdc5889ecc7671db62338462e49b1
Jaideep <jaideepshetti55@gmail.com>
2026-05-07 21:44:29 +0530

    commit 4

---
+++ may07/RandomNumber.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+
+        Random r = new Random();
+
+        System.out.println(r.nextInt(100));
+    }
+}
