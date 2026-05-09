66fa1b024d451291bdeda279f703312d4d1fd212
Jaideep <jaideepshetti55@gmail.com>
2026-05-09 21:55:14 +0530

    commit 3

---
+++ may09/PerfectSquareCheck.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PerfectSquareCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int sqrt = (int)Math.sqrt(n);
+
+        if (sqrt * sqrt == n)
+            System.out.println("Perfect Square");
+        else
+            System.out.println("Not Perfect Square");
+    }
+}
