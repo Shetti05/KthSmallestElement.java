92d2c6959820667f7994dd2ede9bcad535c6dcd3
Jaideep <jaideepshetti55@gmail.com>
2026-05-10 21:35:37 +0530

    commit 8

---
+++ may10/VowelCounter.java
@@ -0,0 +1,18 @@
+public class VowelCounter {
+    public static void main(String[] args) {
+        String text = "Education";
+        int count = 0;
+
+        text = text.toLowerCase();
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+
+            if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+                count++;
+            }
+        }
+
+        System.out.println("Vowels: " + count);
+    }
+}
