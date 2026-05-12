6569fc602b6ff3217ea091ac3c185701984c6c76
Jaideep <jaideepshetti55@gmail.com>
2026-05-12 22:33:41 +0530

    commit 1

---
+++ may12/PrimeNumbersInRange.java
@@ -0,0 +1,19 @@
+public class PrimeNumbersInRange {
+    public static void main(String[] args) {
+        int start = 1, end = 50;
+
+        for (int i = start; i <= end; i++) {
+            int count = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
