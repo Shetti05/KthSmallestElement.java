3e6fb659db96e13b1540e4cba364239528ca9114
Jaideep <jaideepshetti55@gmail.com>
2026-05-13 22:43:23 +0530

    commit 4

---
+++ may13/UppercaseCount.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UppercaseCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i))) {
+                count++;
+            }
+        }
+
+        System.out.println("Uppercase Letters: " + count);
+    }
+}
