912206f1be1e05198e9eaabd66a122e089c1c776
Jaideep <jaideepshetti55@gmail.com>
2026-05-16 21:21:46 +0530

    commit 2

---
+++ may16/WordCounter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String str = sc.nextLine();
+
+        String[] words = str.trim().split("\\s+");
+
+        System.out.println("Total words: " + words.length);
+    }
+}
